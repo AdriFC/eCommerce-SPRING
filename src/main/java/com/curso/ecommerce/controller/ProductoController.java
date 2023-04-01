1632c23d18199ff56efcc6ca59dde4e0551e9c13
AdriFC <adri86vigo@gmail.com>
2023-04-01 11:04:54 +0200

    Vid-18. Funcionalidad para mostrar los productos en la home del administrador.

--- src/main/java/com/curso/ecommerce/controller/ProductoController.java
+++ src/main/java/com/curso/ecommerce/controller/ProductoController.java
@@ -76,14 +76,15 @@ public class ProductoController {
 	@PostMapping("/update")
 	public String update(Producto producto, @RequestParam("img") MultipartFile file) throws IOException {
 		
+		Producto p = new Producto();
+		p=productoService.get(producto.getId()).get();
+		
 		if (file.isEmpty()) { //editamos el producto pero no cambiamos la imagen
-			Producto p = new Producto();
-			p=productoService.get(producto.getId()).get();
+			
 			producto.setImagen(p.getImagen());
 		} else {//Cuando también se edita la imagen
 			
-			Producto p = new Producto();
-			p = productoService.get(producto.getId()).get();
+			
 			
 			//Eliminar la imagen cuando no sea la imagen por defecto
 			if (!p.getImagen().equals("default.jpg")) {
@@ -93,7 +94,7 @@ public class ProductoController {
 			String nombreImagen = upload.saveImage(file);
 			producto.setImagen(nombreImagen);
 		}
-		
+		producto.setUsuario(p.getUsuario());
 		productoService.update(producto);
 		return "redirect:/productos";
 	}
