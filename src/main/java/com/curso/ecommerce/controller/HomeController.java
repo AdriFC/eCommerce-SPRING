609a998bf661c8ff45637700c4b0dfe4c42de92a
AdriFC <adri86vigo@gmail.com>
2023-04-18 19:40:40 +0200

    Vid-28. Funcionalidad para mostrar el contenido del carrito la opción carrito del menú.

--- src/main/java/com/curso/ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/controller/HomeController.java
@@ -76,7 +76,13 @@ public class HomeController {
 		detalleOrden.setTotal(producto.getPrecio()*cantidad);
 		detalleOrden.setProducto(producto);
 		
-		detalles.add(detalleOrden);
+		//Validar que el producto no se añada más de una vez
+		Integer idProducto = producto.getId();
+		boolean ingresado = detalles.stream().anyMatch(p -> p.getProducto().getId() == idProducto);
+		
+		if (!ingresado) {
+			detalles.add(detalleOrden);
+		}
 		
 		sumaTotal = detalles.stream().mapToDouble(dt->dt.getTotal()).sum();
 		
@@ -113,6 +119,15 @@ public class HomeController {
 		
 		
 		return "usuario/carrito";
+	}
+	
+	@GetMapping("/getCart")
+	public String getCart(Model model) {
+		
+		model.addAttribute("cart", detalles);
+		model.addAttribute("orden", orden);
+		
+		return "/usuario/carrito";
 	}
 	
 }
